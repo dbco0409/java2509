888c90a10d62968fe6e44e243fa32ffe13bc01fb
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-09-01 17:40:55 +0900

    Add files via upload

---
+++ java0901/src/java0901/FloatExample.java
@@ -0,0 +1,24 @@
+package java0901;
+
+public class FloatExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float var1 = 0.123456789f;
+		double var2 = 0.123456789;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		double var3 = 3e6;
+		float var4 = 3e6F;
+		double var5 = 2e-3;
+		System.out.println("var3 : "+var3);
+		System.out.println("var4 : "+var4);
+		System.out.println("var5 : "+var5);
+		
+		
+	}
+
+}
---
+++ java0901/src/java0901/ScannerExample.java
@@ -0,0 +1,39 @@
+package java0901;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("x값 입력: ");
+		String strX = scanner.nextLine();
+		int x = Integer.parseInt(strX);
+		
+		System.out.println("Y값 입력: ");
+		String strY = scanner.nextLine();
+		int y = Integer.parseInt(strY);
+		
+		int result = x + y;
+		System.out.println("x+y = "+result);
+		System.out.println();
+		
+		while(true) {
+			System.out.println("입력 문자열:");
+			String data = scanner.nextLine();
+			if(data.equals('q')) {
+				break;
+			}
+			System.out.println("출력 문자열:"+data);
+			System.out.println();
+		}
+		
+		System.out.println("종료");
+		
+		
+		
+	}
+
+}
