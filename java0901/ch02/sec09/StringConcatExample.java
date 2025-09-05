d578e153c57540c046f1f19ea4b27fdbf844f7b5
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-09-05 16:27:30 +0900

    java0901

---
+++ java0901/ch02/sec09/StringConcatExample.java
@@ -0,0 +1,28 @@
+package java0901;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int result11 = 10 + 2 + 8;
+		System.out.println(result11);
+		
+		String result22 = 10 + 2 + "8";
+		System.out.println(result22);
+		
+		String result33 = 10 + "2" + 8;
+		System.out.println(result33);
+		
+		String result44 = "10" + 2 + 8;
+		System.out.println(result44);
+		
+		String result55 = "10" + (2+8);
+		System.out.println(result55);
+		
+		
+	}
+
+}
---
+++ java0901/ch02/sec10/PrimitiveAndStringConversionExample.java
@@ -0,0 +1,32 @@
+package java0901;
+
+import java.util.Scanner;
+
+public class PrimitiveAndStringConversionExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int value1 = Integer.parseInt("10");
+		double value2 = Double.parseDouble("3.14");
+		boolean value3 = Boolean.parseBoolean("true");
+		
+		System.out.println("value1:"+value1);
+		System.out.println("value2:"+value2);
+		System.out.println("value3:"+value3);
+		
+		String str1 = String.valueOf(10);
+		String str2 = String.valueOf(3.14);
+		String str3 = String.valueOf(true);
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		
+		
+		
+	}
+
+}
