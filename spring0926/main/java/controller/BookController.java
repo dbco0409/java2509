e7f4f944633f82e0ff44ecb301e39a39027cc2a6
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-09-26 11:36:38 +0900

    Add files via upload

---
+++ spring0926/main/java/controller/BookController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import com.springboot.service.BookService;
+import com.springboot.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping(value = "/books", method= RequestMethod.GET)
+    public String requestBookList(Model model){
+        List<Book> list=bookService.getAllBookList();
+        model.addAttribute("bookList",list);
+        return "books";
+    }
+}
